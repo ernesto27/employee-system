3743d4d6f40b3d3279056aa965a02ce4cde3ba44
ernesto <ernestoponce27@gmail.com>
2024-12-26 02:18:13 -0300

    enhance backup script

--- backups/backup-db.sh
+++ backups/backup-db.sh
@@ -7,10 +7,15 @@ TIMESTAMP=$(date +"%F_%T")
 BACKUP_FILE="${BACKUP_PATH}/${DATABASE_NAME}_${TIMESTAMP}.sql"
 PASSWORD=${DB_PASSWORD}
 
+# Remove backups older than 7 days
 find ${BACKUP_PATH} -name '*.sql' -mtime +7 -exec rm -f {} \;
 
+# Create backup sql on folder backups
 docker exec ${CONTAINER_NAME} /usr/bin/mysqldump -u ${DB_USERNAME} --password=${PASSWORD} ${DATABASE_NAME} > ${BACKUP_FILE}
 
+# Compress backup file
 tar -czf "${BACKUP_FILE}.tar.gz" -C ${BACKUP_PATH} $(basename ${BACKUP_FILE})
+
+# Remove backup file
 rm ${BACKUP_FILE}
 
